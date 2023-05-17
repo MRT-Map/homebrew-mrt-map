e1368f0de872aa7cefd4cc03c2fd397f64e92002
7d <61975820+iiiii7d@users.noreply.github.com>
2023-05-17 22:06:08 +0800

    styling

--- Casks/stencil2.rb
+++ Casks/stencil2.rb
@@ -1,10 +1,11 @@
 cask "stencil2" do
   version "2.0.5"
+  sha256 "0c77bbc4b58b6dd53f5aca43bb0e35a040f90e2bf0bbcd2eaa67af316a49cc25"
+
   url "https://github.com/MRT-Map/stencil2/releases/download/v#{version}/stencil2.dmg"
   name "stencil2"
   desc "Map editor for MRT Map data"
   homepage "https://github.com/MRT-Map/stencil2"
-  sha256 "0c77bbc4b58b6dd53f5aca43bb0e35a040f90e2bf0bbcd2eaa67af316a49cc25"
 
   auto_updates true
 
